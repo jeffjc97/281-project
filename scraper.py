91649fec9310289f0cf48c9ff3482d4acc37f3ba
Kevin Zhang <kevin.zhang500@gmail.com>
2017-11-28 10:29:50 -0500

    Data: got some data, not all

--- scraper.py
+++ scraper.py
@@ -16,41 +16,41 @@ RANDOM_SONG_FEATURES = "data/random_song_features.csv"
 ### scrape data for top artists over the past year
 ###########################################################################
 
-# top artists over the past year
-scrape_top_charts(
-	datetime.datetime(2016, 12, 30),
-	datetime.datetime(2017, 11, 3),
-	BEST_ARTISTS
-)
+# # top artists over the past year
+# scrape_top_charts(
+# 	datetime.datetime(2016, 12, 30),
+# 	datetime.datetime(2017, 11, 3),
+# 	BEST_ARTISTS
+# )
 
-# get top artists' songs
-with open(BEST_ARTISTS, "r") as csvfile:
-	reader = csv.reader(csvfile)
-	artists = list(reader)[1:]
-	artists = {
-		artist[0]: artist[1] for artist in artists
-	}
-	scrape_top_tracks(artists, BEST_SONGS)
+# # get top artists' songs
+# with open(BEST_ARTISTS, "r") as csvfile:
+# 	reader = csv.reader(csvfile)
+# 	artists = list(reader)[1:]
+# 	artists = {
+# 		artist[0]: artist[1] for artist in artists
+# 	}
+# 	scrape_top_tracks(artists, BEST_SONGS)
 
-# get song data for all top songs
-with open(BEST_SONGS, "r") as csvfile:
-	reader = csv.reader(csvfile)
-	songs = list(reader)[1:]
-	song_ids = [song[2] for song in songs]
-	scrape_song_data(song_ids, BEST_SONG_FEATURES)
+# # get song data for all top songs
+# with open(BEST_SONGS, "r") as csvfile:
+# 	reader = csv.reader(csvfile)
+# 	songs = list(reader)[1:]
+# 	song_ids = [song[2] for song in songs]
+# 	scrape_song_data(song_ids, BEST_SONG_FEATURES)
 
 ###########################################################################
 ### scrape data for random artists since 1990
 ###########################################################################
 
-# get random artists
-scrape_artist_list(RANDOM_ARTISTS, limit=100)
+# # get random artists
+# scrape_artist_list(RANDOM_ARTISTS, limit=1000)
 
-# get random artists' songs
-with open(RANDOM_ARTISTS, "r") as csvfile:
-	reader = csv.reader(csvfile); next(reader, None)
-	artists = { row[0]: row[1] for row in reader }
-	scrape_top_tracks(artists, RANDOM_SONGS)
+# # get random artists' songs
+# with open(RANDOM_ARTISTS, "r") as csvfile:
+# 	reader = csv.reader(csvfile); next(reader, None)
+# 	artists = { row[0]: row[1] for row in reader }
+# 	scrape_top_tracks(artists, RANDOM_SONGS)
 
 # get song data for all random songs
 with open(RANDOM_SONGS, "r") as csvfile:
--- scrapers/song_info.py
+++ scrapers/song_info.py
@@ -1,5 +1,6 @@
-import csv, datetime
+import csv, datetime, dill, requests, time
 from utils import get_client
+from multiprocessing import Pool
 
 BATCH_SIZE = 50
 CLIENT = get_client()
@@ -45,12 +46,12 @@ def _get_values(dict):
 	return list(map(lambda h: dict[h], HEADERS))
 
 albums = {}
-def _get_album(album_id):
+def _get_album(client, album_id):
 	if album_id in albums:
 		return albums[album_id]
-	album = CLIENT.album(album_id)
+	album = client.album(album_id)
 	date = album['release_date'].split("-")
-	year, month, day = int(date[0]) - 1990, int(date[1]), int(date[2])
+	year, month, day = int(date[0]) - 1990, 0 if len(date) < 2 else int(date[1]), 0 if len(date) < 3 else int(date[2])
 	albums[album_id] = {
 		'album_length': len(album['tracks']['items']),
 		'release_date': year * 365 + month * 30 + day
@@ -58,15 +59,15 @@ def _get_album(album_id):
 	return albums[album_id]
 
 artist_popularities = {}
-def _get_popularity(artist_id):
+def _get_popularity(client, artist_id):
 	if artist_id in artist_popularities:
 		return artist_popularities[artist_id]
-	popularity = CLIENT.artist(artist_id)['popularity']
+	popularity = client.artist(artist_id)['popularity']
 	artist_popularities[artist_id] = popularity
 	return popularity
 
-def _get_analysis(analysis_url):
-	analysis = CLIENT.audio_analysis(analysis_url)
+def _get_analysis(client, analysis_url):
+	analysis = client.audio_analysis(analysis_url)
 	return {
 		'end_of_fade_in': 				analysis['track']['end_of_fade_in'],
 		'start_of_fade_out': 			analysis['track']['start_of_fade_out'],
@@ -81,49 +82,40 @@ def _get_analysis(analysis_url):
 		'num_segments': 				len(analysis['segments']),
 	}
 
-def _handle(batch_ids, writer):
-	# get a new SSL connection once in a while
-	CLIENT = get_client()
-	song_info = list(zip(
-		CLIENT.tracks(tracks=batch_ids)['tracks'], 
-		CLIENT.audio_features(tracks=batch_ids)
-	))
-
-	written = 0
-	for song in song_info:
-		data = {
-			'song_id': song[0]['id'],
-			'artist_id': song[0]['artists'][0]['id'],
-			'duration': song[0]['duration_ms'],
-			'popularity': song[0]['popularity'],
-			'explicit': int(song[0]['explicit']),
-			'danceability': song[1]['danceability'],
-			'energy': song[1]['energy'],
-			'loudness': song[1]['loudness'],
-			'speechiness': song[1]['speechiness'],
-			'acousticness': song[1]['acousticness'],
-			'instrumentalness': song[1]['instrumentalness'],
-			'liveness': song[1]['liveness'],
-			'valence': song[1]['valence'],
-			'tempo': song[1]['tempo'],
-			'time_signature': song[1]['time_signature'],
-			'mode': song[1]['mode'],
-			'key': song[1]['key'],
-			'track_number': song[0]['track_number'],
-		}
-		data['artist_popularity'] = _get_popularity(song[0]['artists'][0]['id'])
-		
-		album_data = _get_album(song[0]['album']['id'])
-		analytics = _get_analysis(song[0]['id'])
+def handle_song(song):
+	if song is None or song[0] is None or song[1] is None:
+		return None
+	client = get_client()
+	data = {
+		'song_id': song[0]['id'],
+		'artist_id': song[0]['artists'][0]['id'],
+		'duration': song[0]['duration_ms'],
+		'popularity': song[0]['popularity'],
+		'explicit': int(song[0]['explicit']),
+		'danceability': song[1]['danceability'],
+		'energy': song[1]['energy'],
+		'loudness': song[1]['loudness'],
+		'speechiness': song[1]['speechiness'],
+		'acousticness': song[1]['acousticness'],
+		'instrumentalness': song[1]['instrumentalness'],
+		'liveness': song[1]['liveness'],
+		'valence': song[1]['valence'],
+		'tempo': song[1]['tempo'],
+		'time_signature': song[1]['time_signature'],
+		'mode': song[1]['mode'],
+		'key': song[1]['key'],
+		'track_number': song[0]['track_number'],
+	}
+	try:
+		data['artist_popularity'] = _get_popularity(client, song[0]['artists'][0]['id'])
+		album_data = _get_album(client, song[0]['album']['id'])
+		analytics = _get_analysis(client, song[0]['id'])
 		data = {**data, **album_data, **analytics}
-		
-		writer.writerow(_get_values(data))
-		print("Wrote data for song {}/{}: {}".format(
-			written + 1,
-			len(song_info), 
-			song[0]['name']
-		))
-		written += 1
+		print("Got data for song {}".format(song[0]['name']))
+		return data
+	except requests.exceptions.SSLError:
+		print("SSL Error!")
+		return None
 
 def scrape_song_data(song_ids, file_name):
 	N_BATCHES = int((len(song_ids) + BATCH_SIZE - 1) / BATCH_SIZE)
@@ -140,4 +132,15 @@ def scrape_song_data(song_ids, file_name):
 		print("Working on batch {:02d} of {:02d}".format(
 			batch_index + 1, N_BATCHES
 		))
-		_handle(batch_ids, writer)
+		song_info = list(zip(
+			CLIENT.tracks(tracks=batch_ids)['tracks'], 
+			CLIENT.audio_features(tracks=batch_ids)
+		))	
+		start_time = time.time()
+		with Pool(processes=8) as pool:
+			for song_data in pool.map(handle_song, song_info, 1):
+				if song_data is None:
+					continue
+				writer.writerow(_get_values(song_data))
+		batch_time = time.time() - start_time
+		print("Batch total time: {:.2f}".format(batch_time))
